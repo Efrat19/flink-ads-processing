0fbcd7878b69f34bb7defac6cbfac4358fe6d490
Efrat Levitan <efrat890089@gmail.com>
2024-02-28 18:29:51 +0200

    sliding window compiles

--- flink-jobs/kotlin/AdsByCitySliding/src/main/kotlin/efrat19/AdsByCitySliding.kt
+++ flink-jobs/kotlin/AdsByCitySliding/src/main/kotlin/efrat19/AdsByCitySliding.kt
@@ -2,25 +2,20 @@ package efrat19
 
 import efrat19.ads.Ad
 import efrat19.ads.AdDeserializationSchema
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
-import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.connector.base.DeliveryGuarantee
-import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema
-import org.apache.flink.connector.kafka.sink.KafkaSink
 import org.apache.flink.connector.kafka.source.KafkaSource
 import org.apache.flink.streaming.api.datastream.DataStream
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.Schema;
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
+import org.apache.flink.table.api.DataTypes
+import org.apache.flink.table.api.EnvironmentSettings
 import org.apache.flink.table.api.Expressions.col
-import org.apache.flink.table.api.TableDescriptor;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.table.KafkaDynamicTableFactory;
+import org.apache.flink.table.api.Expressions.lit
+import org.apache.flink.table.api.Schema
+import org.apache.flink.table.api.Slide
+import org.apache.flink.table.api.TableDescriptor
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment
 
 const val IN_TOPIC = "scraped-ads"
 const val OUT_TOPIC = "ads-by-city-and-year-tmbl-win"
@@ -30,6 +25,7 @@ const val GROUP_ID = "flink-job1"
 const val WIDNOW_SIZE_SEC = 60
 const val WIDNOW_SLIDE_SEC = 10
 const val WATERMARK_SEC = 5
+
 fun main() {
         val env = StreamExecutionEnvironment.getExecutionEnvironment()
         val tableEnv = StreamTableEnvironment.create(env, EnvironmentSettings.inStreamingMode())
@@ -40,19 +36,35 @@ fun main() {
                                         .setTopics(IN_TOPIC)
                                         .setGroupId(GROUP_ID)
                                         .setValueOnlyDeserializer(AdDeserializationSchema())
-                                        .build()                        
+                                        .build()
 
-                                        val stream: DataStream<Ad> =
-                                        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
-                
-                        tableEnv.createTable("sink", createKafkaConnectorDescriptor().build())
+        val watermarks =
+                        WatermarkStrategy.forMonotonousTimestamps<Ad>().withTimestampAssigner {
+                                        ad,
+                                        _ ->
+                                isoToEpochMilli(ad.scraped_time)
+                        }
+        val stream: DataStream<Ad> =
+                        env.fromSource(
+                                        source,
+                                        watermarks,
+                                        "Kafka Source"
+                        )
+
+        tableEnv.createTable("sink", createKafkaConnectorDescriptor().build())
 
         val scrapedAdsTable = tableEnv.fromDataStream(stream)
-        scrapedAdsTable
-                .groupBy(col("city"))
-                .select(col("city"),col("ad_id").count())
-                .filter(col("city").isNotEqual(""))
-                .insertInto("sink").execute()
+        scrapedAdsTable.window(
+                                        Slide.over(lit(60))
+                                                        .every(lit(10))
+                                                        .on(col("scraped_time"))
+                                                        .`as`(lit("w"))
+                        )
+                        .groupBy(col("city"))
+                        .select(col("city"), col("ad_id").count())
+                        .filter(col("city").isNotEqual(""))
+                        .insertInto("sink")
+                        .execute()
 
         env.execute("scraped ads aggregation")
 }
@@ -60,15 +72,24 @@ fun main() {
 
 public fun createKafkaConnectorDescriptor(): TableDescriptor.Builder {
         return TableDescriptor.forConnector("upsert-kafka")
-        .schema(
-                Schema.newBuilder()
-                        .column("city", DataTypes.STRING().notNull())
-                        .column("num_ads", DataTypes.BIGINT())
-                        .primaryKey("city")
-                        .build())
-        .option("key.format", "json")
-        .option("value.format", "json")
-        .option("topic", OUT_TOPIC)
-        .option("properties.bootstrap.servers", K_HOST)
-        .option("properties.group.id", GROUP_ID)
-    }
+                        .schema(
+                                        Schema.newBuilder()
+                                                        .column(
+                                                                        "city",
+                                                                        DataTypes.STRING().notNull()
+                                                        )
+                                                        .column("num_ads", DataTypes.BIGINT())
+                                                        .primaryKey("city")
+                                                        .build()
+                        )
+                        .option("key.format", "json")
+                        .option("value.format", "json")
+                        .option("topic", OUT_TOPIC)
+                        .option("properties.bootstrap.servers", K_HOST)
+                        .option("properties.group.id", GROUP_ID)
+}
+
+fun isoToEpochMilli(iso: String): Long {
+        val dateTime = LocalDateTime.parse(iso)
+        return dateTime.atZone(ZoneOffset.UTC).toInstant().toEpochMilli()
+}
--- flink-jobs/kotlin/AdsByCitySliding/src/main/kotlin/efrat19/ads/Ad.kt
+++ flink-jobs/kotlin/AdsByCitySliding/src/main/kotlin/efrat19/ads/Ad.kt
@@ -1,5 +1,4 @@
 package efrat19.ads
-
 import com.lapanthere.flink.api.kotlin.typeutils.DataClassTypeInfoFactory
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
@@ -18,9 +17,7 @@ public fun AdFactory(s: String): Ad =
 data class Ad(
         val city: String = "",
         val ad_id: String,
-        // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
         val posted_time: String,
-        // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss:SSS")
         val scraped_time: String,
 ) {
     constructor(
@@ -31,70 +28,6 @@ data class Ad(
             AdFactory(adStr).posted_time,
             AdFactory(adStr).scraped_time
     )
+
 }
 
-// package org.apache.flink.playgrounds.ops.clickcount.records
-
-// import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonFormat
-
-// import java.util.Date
-// import java.util.Objects
-
-// /**
-//  * A simple event recording a click on a {@link ClickEvent#page} at time {@link
-// ClickEvent#timestamp}.
-//  *
-//  */
-// public class ClickEvent {
-
-// 	//using java.util.Date for better readability
-// 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss:SSS")
-// 	private var timestamp: Date? = null
-// 	private var page: String? = null
-
-// 	public ClickEvent() {
-// 	}
-
-// 	public ClickEvent(final Date timestamp, final String page) {
-// 		this.timestamp = timestamp;
-// 		this.page = page;
-// 	}
-
-// 	public Date? getTimestamp() {
-// 		return timestamp;
-// 	}
-
-// 	public Fun<Unit> setTimestamp(final Date? timestamp) {
-// 		this.timestamp = timestamp;
-// 		return Fun.unit
-// 	}
-
-// 	public String? getPage() {
-// 		return page;
-// 	}
-
-// 	public Fun<Unit> setPage(final String? page) {
-// 		this.page = page;
-// 		return Fun.unit
-// }
-
-// 	override fun equals(obj: Any?): Boolean {
-// 		if (this === obj) return true
-// 		if (obj == null || java.lang.Class. geothermal(obj?.javaClass)?.toString() != java.lang.Class.
-// geothermal(java.lang.Class. geothermal(this)?.javaClass)?.toString()) return false
-// 		val that = obj as ClickEvent?
-// 		return Objects.equals(timestamp, that!!.timestamp) && Objects.equals(page, that.page)
-// 	}
-
-// 	override fun hashCode(): Int {
-// 		return Objects.hash(timestamp, page)
-// }
-
-// 	override fun toString(): String {
-// 		val sb = StringBuilder("ClickEvent{")
-// 		sb.append("timestamp=$timestamp")
-// 		sb.append(", page=$page")
-// 		sb.append("}")
-// 		return sb.toString()
-// }
-// }
